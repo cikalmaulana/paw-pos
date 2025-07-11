27c0a012148181bf5da00cd18f812bc34f903c8a
cikalmaulana <rajasacikalmaulana@gmail.com>
2025-07-11 11:05:41 +0700

    [UPDATE] - Delete Low Stock

--- app/account/manage/item/manage.item.main.tsx
+++ app/account/manage/item/manage.item.main.tsx
@@ -11,7 +11,6 @@ import AddNewItem from "./add/item.add.main";
 const ManageItemView = lazy(() => import("./view/view.main"));
 const ManageItemEditStock = lazy(() => import("./stock/stock.main"))
 const ManageItemCategories = lazy(() => import("./category/category.main"))
-const LowStockMain = lazy(() => import("./lowstock/lowstock.main"))
 
 interface I_Props {
     handleBack:()=>void
@@ -26,8 +25,7 @@ export default function ManageItem(props: I_Props){
         { key: 'viewItem', label: 'View all item', image:require("@/assets/icons/viewItem.png") },
         { key: 'addItem', label: 'Add new item', image:require("@/assets/icons/addItem.png") },
         { key: 'editStock', label: 'Edit stock', image:require("@/assets/icons/editStock.png")  },
-        { key: 'categories', label: 'Categories', image:require("@/assets/icons/categories.png")  },
-        { key: 'lowStock', label: 'View low stock', image:require("@/assets/icons/lowStock.png")  },
+        { key: 'categories', label: 'Categories', image:require("@/assets/icons/categories.png")  }
     ]
 
     const [manageItemOpen, setManageItemOpen] = useState('')
@@ -76,11 +74,11 @@ export default function ManageItem(props: I_Props){
                         contentContainerStyle={{ paddingBottom: 500 }}
                     >
                         <View className="flex flex-row gap-4 w-full mb-8 px-2">
-                            <CE_Card className="bg-primary p-5 flex justify-center flex-1">
+                            <CE_Card className="bg-primary p-5 flex justify-center flex-1" onPress={() => setManageItemOpen('viewItem')}>
                                 <Text className="text-white font-semibold text-lg">Total Item</Text>
                                 <Text className="text-white font-semibold text-3xl">{totalItem}</Text>
                             </CE_Card>
-                            <CE_Card className="bg-white p-5 flex justify-center flex-1">
+                            <CE_Card className="bg-white p-5 flex justify-center flex-1" onPress={() => setManageItemOpen('editStock')}>
                                 <Text className="text-primary font-semibold text-lg">Total Low Stock</Text>
                                 <Text className="text-primary font-semibold text-3xl">4</Text>
                             </CE_Card>
@@ -118,22 +116,11 @@ export default function ManageItem(props: I_Props){
                         setAlertSuccess={props.setAlertSuccess}
                     />
                 </Suspense>
-            ) : manageItemOpen === 'categories' ? (
+            ) : manageItemOpen === 'categories' && (
                 <Suspense fallback={
                     <CE_Loading />
                 }>
                     <ManageItemCategories 
-                        handleBack={() => setManageItemOpen('')}
-                        setShowAlert={props.setShowAlert}
-                        setAlertMsg={props.setAlertMsg}
-                        setAlertSuccess={props.setAlertSuccess}
-                    />
-                </Suspense>
-            ) : manageItemOpen === 'lowStock' && (
-                <Suspense fallback={
-                    <CE_Loading />
-                }>
-                    <LowStockMain 
                         handleBack={() => setManageItemOpen('')}
                         setShowAlert={props.setShowAlert}
                         setAlertMsg={props.setAlertMsg}
